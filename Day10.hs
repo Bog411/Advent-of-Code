d7f12bd9febad991aa4977f45dc9eb859d050da2
Bog411 <Bog411@gmail.com>
2015-12-10 01:57:03 -0600

    Update Day10.hs

--- Day10.hs
+++ Day10.hs
@@ -4,7 +4,7 @@ import Data.List
 
 input = [1,1,1,3,2,2,2,1,1,3,]
 f z = zip (map length $ group z) (map head $ group z)
-g = concatMap (\(a,b) -> [a,b,]) 
+g = concatMap (\(a,b) -> [a,b]) 
 
 part1 = (!! 40) . iterate ( g.f ) 
 length part1 input
